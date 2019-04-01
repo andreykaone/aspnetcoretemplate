34d15bcb3587f003f46c3d093abdc0c5d780dc32
kormilitsyn.andrey <andreyka.one.two@gmail.com>
2019-04-02 01:04:47 +0300

    initial commit

---
+++ aspnetcoretemplate/aspnetcoretemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnetcoretemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
